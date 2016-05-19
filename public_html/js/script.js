a8375a7a4b2c5a710e23a8365060ff7c42cd0a22
Jan-René Grünhagen <gruenhagen@strebergarten.org>
2016-05-19 12:59:35 +0200

    1.2&1.3 rdy; 1.1 retry

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -5,9 +5,13 @@ var n=prompt("Wieviele Zahlen im Array?");
 var a=prompt("Wo starten?");
 var b=prompt("Wo enden?");
 function fillarray(a,b,n) {
+    console.log("a= "+a);
+    console.log("b= "+b);
+    console.log("n= "+n);
+    
     for (var i=0;i<n;i++) {
-        zahlen[i]=Math.floor(Math.random()*b+a);
-        console.log(zahlen[i]);
+        zahlen[i]=Math.floor((Math.random()*b)+a);
+        console.log("schleifendurchlauf "+i+" mit dem Wert: "+zahlen[i]);
     }        
         return zahlen;
 }
@@ -15,3 +19,24 @@ fillarray(a,b,n);
 
 
 //1.2
+function alleAusgeben(zahlen) {
+    for (var i=0; i<zahlen.length;i++) {
+        if (i%10===0)
+            document.write("<br />");
+        document.write(zahlen[i]+", ");
+    }
+}
+
+alleAusgeben(zahlen);
+
+
+//1.3
+function summe(zahlen) {
+    var gesamtsumme=0;
+    for (var i=0; i< zahlen.length; i++) {
+        gesamtsumme+=zahlen[i];
+    }
+    return gesamtsumme;
+}
+console.log("gesamtsumme:"+summe(zahlen));
+summe(zahlen);
